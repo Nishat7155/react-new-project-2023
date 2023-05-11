b540602293ddcf587a6b4e5fb5618a385b59633a
Nishat7155 <57759578+Nishat7155@users.noreply.github.com>
2023-05-12 00:02:31 +0200

    pages delete
    delete pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import {Browser as Router, Switch, Route} from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import Signup from './pages/SignpPage/Signup';
+
 
 function App() {
 	return (
-		<div>start</div>
+		<Router>
+			<Switch>
+				<Route path='/' exact component={HomePage}/>
+				<Route path='/sign-up' exact component={Signup}/>
+			</Switch>
+		</Router>
 	);
 }
 
--- src/psges/HomePage/HomePage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HomePAge = () => {
-  return (
-    <div>HomePAge</div>
-  )
-}
-
-export default HomePAge
