08390c202d296d4db59ab54b39069257ed132068
Nishat7155 <57759578+Nishat7155@users.noreply.github.com>
2023-05-12 00:07:27 +0200

    app.js
    app.js changed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import {Browser as Router, Switch, Route} from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage';
-import Signup from './pages/SignpPage/Signup';
+
 
 
 function App() {
 	return (
 		<Router>
-			<Switch>
-				<Route path='/' exact component={HomePage}/>
-				<Route path='/sign-up' exact component={Signup}/>
-			</Switch>
+
 		</Router>
 	);
 }
