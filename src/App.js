1728c4ef1bfd5a5f1ed62573ba9edcd5e63bc03f
Nishat7155 <57759578+Nishat7155@users.noreply.github.com>
2023-05-12 18:30:05 +0200

    router-dom
    globalStyle design and other folder router fixed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route
+	
+  } from "react-router-dom";
+import Home from './pages/Home/Home';
+import Signup from './pages/SignUp/Signup';
 
 function App() {
 	return (
-		<> </>
+		<Router>
+			<Switch>
+			    <Route path="/" exact component={Home} />
+			    <Route path="/sign-up" exact component={Signup} />
+			</Switch>
+		</Router>
 	);
 }
 
